d8ae65e64405a8f080ca092906896cc9be2eee72
phil.yue@foxmail.com <phil.yue@foxmail.com>
2017-03-09 19:36:01 +0800

    ceshi

--- src/main/java/cn/philalina/PhilAlinaApplication.java
+++ src/main/java/cn/philalina/PhilAlinaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PhilAlinaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("my love alina");
 		SpringApplication.run(PhilAlinaApplication.class, args);
 	}
 }
